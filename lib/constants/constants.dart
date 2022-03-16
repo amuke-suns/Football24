b600547eab7f43134d11faba3cf04d8ec52699bc
Amuke Sunday <sundayamuke7@gmail.com>
2022-03-16 11:43:32 +0100

    Added a file for constants.

---
+++ lib/constants/constants.dart
@@ -0,0 +1,12 @@
+// the whitespace is to ensure it's top of the grouped list view
+const kFavouriteKey = ' FAVOURITE COMPETITIONS';
+const kOtherCompetitionsKey = 'OTHER COMPETITIONS [A-Z]';
+
+final kFavIdList = [
+  2, // ucl
+  39, // epl
+  140, // la liga
+  135, // serie a
+  78, // bundesliga
+  61, // ligue 1
+];
