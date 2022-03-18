f1bd994d315b5bccc2db95d0e73a354143131f51
Amuke Sunday <sundayamuke7@gmail.com>
2022-03-18 21:35:32 +0100

    Implemented required refactoring

--- lib/ui/fixtures_screen.dart
+++ lib/ui/fixtures_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:football_news/models/models.dart';
 import 'package:football_news/network/news_fixture_model.dart';
+import 'package:football_news/widgets/custom_appbar_title.dart';
+import 'package:football_news/widgets/header_text_container.dart';
+import 'package:provider/provider.dart';
 
 class FixturesScreen extends StatelessWidget {
   final String title;
@@ -11,54 +17,99 @@ class FixturesScreen extends StatelessWidget {
     required this.fixtures,
   }) : super(key: key);
 
+  Widget _buildTeamScoreRow({
+    required String teamName,
+    int? score,
+    bool? isWinner,
+    required String teamLogo,
+  }) {
+    return Row(
+      children: [
+        Image.network(
+          teamLogo,
+          width: 24,
+        ),
+        const SizedBox(width: 5),
+        Expanded(
+          child: Text(
+            teamName,
+            style: TextStyle(
+              fontWeight: isWinner != null && isWinner ? FontWeight.bold : null,
+            ),
+          ),
+        ),
+        Text(
+          score?.toString() ?? '-',
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: ListView.builder(
-        itemCount: fixtures.length,
-        itemBuilder: (BuildContext context, int index) {
-          APIFixtureDetails fixture = fixtures[index];
-          bool? isHomeWinner = fixture.teams.home.winner;
+    bool darkMode =
+        Provider.of<SettingsManager>(context, listen: true).darkMode;
+    var league = fixtures.first.league;
 
-          return ListTile(
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      appBar: AppBar(
+        title: CustomAppBarTitle(
+          title: title,
+          subtitle: null,
+        ),
+      ),
+      body: Column(
+        children: [
+          FavouriteHeaderTextClicker(
+            isDarkMode: darkMode,
+            country: league.country,
+            league: league.name,
+            imageUrl: league.flag,
+            onTap: () {
+              print('clicked');
+            },
+          ),
+          Flexible(
+            child: ListView(
               children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        fixture.teams.home.name,
-                        style: TextStyle(
-                          fontWeight: isHomeWinner != null && isHomeWinner
-                              ? FontWeight.bold
-                              : null,
+                for (var fixture in fixtures)
+                  ListTile(
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _buildTeamScoreRow(
+                          teamName: fixture.teams.home.name,
+                          score: fixture.score.fulltime.home,
+                          isWinner: fixture.teams.home.winner,
+                          teamLogo: fixture.teams.home.logo,
                         ),
-                      ),
-                    ),
-                    Text('${fixture.score.fulltime.home ?? '-'}'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        fixture.teams.away.name,
-                        style: TextStyle(
-                          fontWeight: isHomeWinner != null && !isHomeWinner
-                              ? FontWeight.bold
-                              : null,
+                        const SizedBox(height: 5),
+                        _buildTeamScoreRow(
+                          teamName: fixture.teams.away.name,
+                          score: fixture.score.fulltime.away,
+                          isWinner: fixture.teams.away.winner,
+                          teamLogo: fixture.teams.away.logo,
                         ),
-                      ),
+                      ],
                     ),
-                    Text('${fixture.score.fulltime.away ?? '-'}')
-                  ],
+                  ),
+                ListTile(
+                  leading: const Icon(Icons.format_list_numbered),
+                  minLeadingWidth: 5,
+                  title: const Text('Standings'),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 18,
+                  ),
+                  onTap: () {
+                    print('Standings tapped');
+                  },
                 ),
               ],
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:football_news/models/models.dart';
 import 'package:football_news/ui/all_games_screen.dart';
 import 'package:football_news/ui/favorites_screen.dart';
 import 'package:football_news/ui/live_screen.dart';
 import 'package:football_news/ui/competitions_screen.dart';
+import 'package:football_news/ui/settings_screen.dart';
+import 'package:football_news/widgets/custom_appbar_title.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,47 +16,94 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  static final titles = ['All Games', 'LIVE', 'Favourites', 'Standings'];
+  static final appBarText = [
+    [null, '17.03.2022, THURSDAY'],
+    [null, 'LIVE'],
+    ['FAVOURITES', null],
+    [null, 'Standings'],
+  ];
 
-  final List<Widget> _screens = const [
+  final List<Widget> _pages = const [
     AllGamesScreen(),
     LiveScreen(),
     FavouritesScreen(),
     CompetitionsScreen()
   ];
 
-  int _currentIndex = 0;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Football News')),
-      body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
-        onTap: (index) {
-          setState(() => _currentIndex = index);
-        },
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'All Games',
+    return Consumer<AppStateManager>(builder: (context, manager, child) {
+      final int index = manager.selectedTab;
+
+      return Scaffold(
+        appBar: AppBar(
+          title: CustomAppBarTitle(
+            title: appBarText[index][0],
+            subtitle: appBarText[index][1],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.live_tv),
-            label: 'LIVE',
+          leading: IconButton(
+            icon: const Icon(Icons.settings_outlined),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return SettingsScreen(
+                    darkMode:
+                        Provider.of<SettingsManager>(context, listen: false)
+                            .darkMode,
+                  );
+                }),
+              );
+            },
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.star_border),
-            label: 'Favorites',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.leaderboard),
-            label: 'Standings',
-          ),
-        ],
-      ),
-    );
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                print('search button clicked');
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.calendar_today),
+              onPressed: () {
+                print('calender button clicked');
+              },
+            ),
+          ],
+        ),
+        body: IndexedStack(
+          index: manager.selectedTab,
+          children: _pages,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor:
+              Theme.of(context).textSelectionTheme.selectionColor,
+          type: BottomNavigationBarType.fixed,
+          currentIndex: index,
+          onTap: (index) {
+            manager.goToTab(index);
+          },
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.home),
+              label: titles[0],
+            ),
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.live_tv),
+              label: titles[1],
+            ),
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.star_border),
+              label: titles[2],
+            ),
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.leaderboard),
+              label: titles[3],
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
