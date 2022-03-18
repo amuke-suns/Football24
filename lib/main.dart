7229909a38f7805c73d6fcd61f6908b8f3244119
Amuke Sunday <sundayamuke7@gmail.com>
2022-03-18 21:34:02 +0100

    Added Multiple providers and the custom app theme.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:football_news/football_news_theme.dart';
+import 'package:football_news/models/models.dart';
 import 'package:football_news/ui/home.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const FootballNewsApp());
 }
 
-class FootballNewsApp extends StatelessWidget {
+class FootballNewsApp extends StatefulWidget {
   const FootballNewsApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<FootballNewsApp> createState() => _FootballNewsAppState();
+}
+
+class _FootballNewsAppState extends State<FootballNewsApp> {
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Football News',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AppStateManager()),
+        ChangeNotifierProvider(create: (context) => SettingsManager()),
+      ],
+      child: Consumer<SettingsManager>(
+        builder: (context, manager, child) {
+          ThemeData theme;
+          if (manager.darkMode) {
+            theme = FootballNewsTheme.dark();
+          } else {
+            theme = FootballNewsTheme.light();
+          }
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Football News',
+            theme: theme,
+            home: const Home(),
+          );
+        },
       ),
-      home: const Home(),
     );
   }
 }
