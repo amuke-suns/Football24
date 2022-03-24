c0ff9dcc835a00cdd191beed7b083d4c1ab7bc5e
Amuke Sunday <sundayamuke7@gmail.com>
2022-03-24 08:05:20 +0100

    Settings screen and manager now use the app cache

--- lib/models/settings_manager.dart
+++ lib/models/settings_manager.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:football_news/models/app_cache.dart';
 
 class SettingsManager extends ChangeNotifier {
-  bool _darkMode = true;
+  final _appCache = AppCache();
+
+  late bool _darkMode = true;
   bool get darkMode => _darkMode;
 
   set darkMode(bool darkMode) {
     _darkMode = darkMode;
+    _appCache.cacheDarkMode(darkMode);
     notifyListeners();
+  }
+
+  void init() async {
+    _darkMode = await _appCache.isDarkMode();
   }
 }
