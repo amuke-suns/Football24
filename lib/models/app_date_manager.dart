a337af503d81942f3314c4810f424782e0901274
Amuke Sunday <sundayamuke7@gmail.com>
2022-03-24 08:00:31 +0100

    A model for date formatting and the AppDate ChangeNotifier

---
+++ lib/models/app_date_manager.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:football_news/models/models.dart';
+import 'package:intl/intl.dart';
+
+class AppDateManager extends ChangeNotifier {
+
+  List<int> _years = [];
+  List<int> get years => _years;
+
+  // supported days
+  List<AppDate> _days = [];
+  List<AppDate> get days => List.unmodifiable(_days);
+  int _selectedDayIndex = 0;
+
+  int get selectedDayIndex => _selectedDayIndex;
+  set selectedDayIndex(int index) {
+    _selectedDayIndex = index;
+    notifyListeners();
+  }
+
+  void init() {
+    setDateList();
+    setYearList();
+  }
+
+  void setYearList() {
+    _years = [];
+    int year = DateTime.now().year;
+
+    for (int i = 2010; i < year; i++) {
+      _years.add(i);
+    }
+    // _selectedYearIndex = _years.length - 1;
+  }
+
+  // int getSelectedYear() => years[_selectedYearIndex];
+
+  String getYearTimeline(int year) {
+    return '$year/${year + 1}';
+  }
+
+  // String getSelectedYearTimeline() => getYearTimeline(selectedYearIndex);
+
+  void setDateList() {
+    _days = [];
+
+    var date = DateTime.now();
+    final DateFormat formatter = DateFormat('dd.MM');
+
+    String todayDateString = formatter.format(date);
+    date = date.add(const Duration(days: -8));
+
+    for (int i = 0; i < 15; i++) {
+      date = date.add(const Duration(days: 1));
+      String dateString = formatter.format(date);
+
+      if (dateString == todayDateString) {
+        _selectedDayIndex = i;
+      }
+
+      _days.add(AppDate(
+        day: date.day,
+        month: date.month,
+        year: date.year,
+        weekday: date.weekday,
+        isToday: dateString == todayDateString,
+      ));
+    }
+  }
+
+  String getSelectedDateApiFormat() {
+    return days[selectedDayIndex].apiFormat();
+  }
+
+  String? getAppDateDescription({required int selectedTab}) {
+    switch (selectedTab) {
+      case FootballNewsTab.allGames:
+        return days[selectedDayIndex].detailedFormat();
+      case FootballNewsTab.live:
+        return FootballNewsTab.tabTitles[selectedTab];
+      case FootballNewsTab.favorites:
+        return null;
+      case FootballNewsTab.standings:
+        return FootballNewsTab.tabTitles[selectedTab];
+      default:
+        return null;
+    }
+  }
+}
