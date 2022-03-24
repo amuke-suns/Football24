b966667261f85ccf85fa26ddff0194f78be05168
Amuke Sunday <sundayamuke7@gmail.com>
2022-03-24 08:06:02 +0100

    Updated barrel files

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,2 +1,5 @@
 export 'app_state_manager.dart';
 export 'settings_manager.dart';
+export 'app_cache.dart';
+export 'app_date.dart';
+export 'app_date_manager.dart';
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,4 @@
 export 'formatted_text.dart';
 export 'competition_card.dart';
-export 'header_text_container.dart';
+export 'headers.dart';
 export 'custom_appbar_title.dart';
