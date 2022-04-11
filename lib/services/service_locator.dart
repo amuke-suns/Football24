47880bf669a1a348574af25392f35a636e584fc2
Amuke Sunday <sundayamuke7@gmail.com>
2022-04-11 23:47:47 +0100

    Removed the view models from get_it instance. Only services are registered to get_it now.

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -1,6 +1,3 @@
-import 'package:football_news/business_logic/view_models/all_games_view_model.dart';
-import 'package:football_news/business_logic/view_models/favourites_view_model.dart';
-import 'package:football_news/business_logic/view_models/settings_view_model.dart';
 import 'package:football_news/services/web_api/web_api_fake.dart';
 import 'package:get_it/get_it.dart';
 
@@ -9,7 +6,6 @@ import 'favourite/favourite_service_impl.dart';
 import 'storage/storage_service.dart';
 import 'storage/storage_service_impl.dart';
 import 'web_api/web_api.dart';
-import 'web_api/web_api_impl.dart';
 
 GetIt serviceLocator = GetIt.instance;
 
@@ -21,18 +17,4 @@ void setupServiceLocator() {
   serviceLocator
       .registerLazySingleton<FavouriteService>(() => FavouriteServiceImpl());
 
-  // serviceLocator.isReady()
-  /*serviceLocator.registerFactoryAsync<FavouritesViewModel>(
-      () async => FavouritesViewModel().init());
-  serviceLocator.registerFactoryAsync<SettingsViewModel>(
-      () async => SettingsViewModel().init());
-  serviceLocator.registerFactoryAsync<AllGamesViewModel>(
-          () async => AllGamesViewModel().init());*/
-
-  serviceLocator.registerFactory<FavouritesViewModel>(
-          () => FavouritesViewModel()..init());
-  serviceLocator.registerFactory<SettingsViewModel>(
-          () => SettingsViewModel()..init());
-  serviceLocator.registerFactory<AllGamesViewModel>(
-          () => AllGamesViewModel()..init());
 }
